addf05c124e7326722096ed596b1417105990989
Simon C <CASTANOSIMON2002@GMAIL.COM>
2023-03-31 19:40:17 -0500

    asdad

--- resources/js/Components/DropdownLink.vue
+++ resources/js/Components/DropdownLink.vue
@@ -6,7 +6,7 @@ defineProps({
     as: String,
 });
 </script>
-
+ 
 <template>
     <div>
         <button v-if="as == 'button'" type="submit" class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
